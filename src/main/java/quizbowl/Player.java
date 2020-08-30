f4c31cb8096b4bbda574bb14cfbf43d8739d2020
Jason <27450071+jyao6429@users.noreply.github.com>
2020-08-30 15:44:05 -0400

    Mostly done updating Player and Team

--- src/main/java/quizbowl/Player.java
+++ src/main/java/quizbowl/Player.java
@@ -1,19 +1,55 @@
 package quizbowl;
 
 import net.dv8tion.jda.api.entities.Member;
-import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
 
 public class Player implements Comparable<Player>
 {
 	private Member member;
 	private int score, tens, powers, negs, bonuses;
-	private Scoreboard scoreboard;
+	private Match match;
+	private Team team;
 
-	public Player(Member m, Scoreboard s)
+	public Player(Member m, Match s, Team t)
 	{
+		team = t;
 		member = m;
-		scoreboard = s;
+		match = s;
 	}
+	public Player(Member m, Match s)
+	{
+		this(m, s, null);
+	}
+	public void add(Match.Points points)
+	{
+		add(points, false);
+	}
+	public void add(Match.Points points, boolean subtract)
+	{
+		int toAdd = subtract ? -1 : 1;
+		switch (points)
+		{
+			case TEN:
+				tens += toAdd;
+				toAdd *= 10;
+				break;
+			case POWER:
+				powers += toAdd;
+				toAdd *= 15;
+				break;
+			case NEG:
+				negs += toAdd;
+				toAdd *= -5;
+				break;
+			case BONUS:
+				bonuses += toAdd;
+				toAdd *= 10;
+				break;
+		}
+		score += toAdd;
+	}
+	/*
 	public void add(int toAdd)
 	{
 		add(toAdd, false);
@@ -49,30 +85,60 @@ public class Player implements Comparable<Player>
 		}
 		score += toAdd;
 	}
+	 */
 	public int getScore()
 	{
 		return score;
+	}
+	public int getTens()
+	{
+		return tens;
+	}
+	public int getPowers()
+	{
+		return powers;
+	}
+	public int getNegs()
+	{
+		return negs;
+	}
+	public int getBonuses()
+	{
+		return bonuses;
 	}
 	public Member getMember()
 	{
 		return member;
 	}
-	public Scoreboard getScoreboard()
+	public Match getMatch()
 	{
-		return scoreboard;
+		return match;
 	}
-	public int compareTo(@NotNull Player o)
+	public Team getTeam()
+	{
+		return team;
+	}
+	public int compareTo(Player o)
 	{
 		return o.getScore() - this.getScore();
 	}
 	public String toString()
 	{
 		String toReturn = String.format("%s - **%d** pts (15: **%d** | 10: **%d** | -5: **%d**", member.getAsMention(), score, powers, tens, negs);
-		if (scoreboard.getSession().getNumOfBonuses() > 0)
+		if (match.getNumOfBonuses() > 0)
 		{
 			toReturn += " | B: **" + bonuses + "**";
 		}
 		toReturn += ")";
 		return toReturn;
+	}
+	@Override public boolean equals(Object o)
+	{
+		if (this == o)
+			return true;
+		if (!(o instanceof Player))
+			return false;
+		Player player = (Player) o;
+		return member.equals(player.member);
 	}
 }
---
+++ src/main/java/quizbowl/Team.java
@@ -0,0 +1,96 @@
+package quizbowl;
+
+import net.dv8tion.jda.api.entities.Role;
+
+import java.util.ArrayList;
+
+public class Team implements Comparable<Team>
+{
+	private String name;
+	private Role role;
+	private boolean isRole;
+	private Match match;
+	private ArrayList<Player> players;
+
+	public Team(String n, Match m)
+	{
+		name = n;
+		role = null;
+		isRole = false;
+		match = m;
+		players = new ArrayList<>();
+	}
+	public Team(Role r, Match m)
+	{
+		role = r;
+		name = role.getAsMention();
+		isRole = true;
+		match = m;
+		players = new ArrayList<>();
+	}
+	public int addPlayer(Player p)
+	{
+		if (!players.contains(p))
+		{
+			players.add(p);
+			return 0;
+		}
+		return -1;
+	}
+	public void removePlayer(Player p)
+	{
+		players.remove(p);
+	}
+	public int getScore()
+	{
+		int sum = 0;
+		for (Player temp : players)
+		{
+			sum += temp.getScore();
+		}
+		return sum;
+	}
+	public int compareTo(Team o)
+	{
+		return o.getScore() - this.getScore();
+	}
+	public String toString()
+	{
+		int score, tens, powers, negs, bonuses;
+		score = tens = powers = negs = bonuses = 0;
+		for (Player temp : players)
+		{
+			score += temp.getScore();
+			tens += temp.getTens();
+			powers += temp.getPowers();
+			negs += temp.getNegs();
+			bonuses += temp.getBonuses();
+		}
+		String toReturn = String.format("%s - **%d** pts (15: **%d** | 10: **%d** | -5: **%d** | B: **%d**)", name, score, powers, tens, negs, bonuses);
+		return toReturn;
+	}
+	public int getNumOfPlayers()
+	{
+		return players.size();
+	}
+	public ArrayList<Player> getPlayers()
+	{
+		return players;
+	}
+	public Match getMatch()
+	{
+		return match;
+	}
+	public String getName()
+	{
+		return name;
+	}
+	public Role getRole()
+	{
+		return role;
+	}
+	public boolean getIsRole()
+	{
+		return isRole;
+	}
+}
